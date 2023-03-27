5dfd0f6e3280000f4aa2e7ab0e22aa05083080cd
Alaa-Hany2000 <hm425038@gmail.com>
2023-03-27 22:42:40 +0200

    s

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -127,11 +127,11 @@
         $adminList = [
 
 
-            [
-                // "href" => ('/admin/settings'),
-                // "title" => trans("main.Settings"),
-                // "icon" => "fa-cog"
-            ],
+            // [
+            //     "href" => ('/admin/settings'),
+            //     "title" => trans("main.Settings"),
+            //     "icon" => "fa-cog"
+            // ],
             /* ["href" => "#", "title" => trans("main.adminSalaries"), "icon" => "fa-newspaper", "child" => [["href" => ("admin/adminSalaries"), "title" => trans("main.adminSalaries"), "icon" => "fa-newspaper", ] ] ], ["href" => "#", "title" => trans("main.adminSalaries"), "icon" => "fa-newspaper", "child" => [["href" => ("admin/adminSalaries"), "title" => trans("main.adminSalaries"), "icon" => "fa-newspaper", ] ] ],*/ [
                 "href" => ('/admin/listUsers'),
                 "title" => trans("main.User Management"),
